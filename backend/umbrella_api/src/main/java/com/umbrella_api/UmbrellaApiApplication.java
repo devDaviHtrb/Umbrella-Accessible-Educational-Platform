a49e09b285a95202c0543f18628f016c1466d970
devDaviHtrb <davideleteribeiro1@gmail.com>
2026-04-19 01:57:11 -0300

    <add> react native and spring boot projects

---
+++ backend/umbrella_api/src/main/java/com/umbrella_api/UmbrellaApiApplication.java
@@ -0,0 +1,13 @@
+package com.umbrella_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbrellaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbrellaApiApplication.class, args);
+	}
+
+}
---
+++ backend/umbrella_api/src/test/java/com/umbrella_api/UmbrellaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umbrella_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbrellaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
