9f4bc860b5d0ca2edba7db780d2286cba334452d
devDaviHtrb <davideleteribeiro1@gmail.com>
2026-06-17 09:11:20 -0300

    <fix> fileMetaData model

--- backend/umbrella_api/src/main/java/com/umbrella_api/modules/storage/model/FileMetaData.java
+++ backend/umbrella_api/src/main/java/com/umbrella_api/modules/storage/model/FileMetaData.java
@@ -8,37 +8,46 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Entity
-@Data
 @Table(name = "files")
+@Getter
+@Setter
 @SuperBuilder
+@NoArgsConstructor  
+@AllArgsConstructor
 public class FileMetaData {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id; 
 
     @Column(nullable = false)
-    String title;
+    private String title;
 
-    // awaiting the data dict
     @Column(nullable = false)
-    String status;
+    private String status;
 
     @Column
-    String description;
+    private String description;
 
-    @Column
-    long size;
+    @Column(nullable = false) 
+    private Long size;
 
     @OneToOne
-    @JoinColumn(name = "image_id", referencedColumnName = "id", nullable = true)
+    @JoinColumn(name = "image_id", referencedColumnName = "id")
     private Image image;
 
     @OneToOne
-    @JoinColumn(name = "raw_id", referencedColumnName = "id", nullable = true)
+    @JoinColumn(name = "raw_id", referencedColumnName = "id")
     private RawFile rawFile;
 
+    @OneToOne
+    @JoinColumn(name = "video_id", referencedColumnName = "id")
+    private Videos videos; 
 }
