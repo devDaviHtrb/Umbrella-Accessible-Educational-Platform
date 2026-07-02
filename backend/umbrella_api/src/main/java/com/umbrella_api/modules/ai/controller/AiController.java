bd09c4f6acee99d7638c8fbb8d0bc6b74b27e94d
devDaviHtrb <davideleteribeiro1@gmail.com>
2026-07-01 23:21:44 -0300

    <fix> ai tutor controller

--- backend/umbrella_api/src/main/java/com/umbrella_api/modules/ai/controller/AiController.java
+++ backend/umbrella_api/src/main/java/com/umbrella_api/modules/ai/controller/AiController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.umbrella_api.modules.ai.api.AiService;
 import com.umbrella_api.modules.ai.dto.AiResponse;
 import com.umbrella_api.modules.ai.service.GeminiService;
 
@@ -13,15 +14,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RestController
 @RequestMapping("/aiTutor")
 public class AiController {
-    private final GeminiService geminiService;
+    private final AiService aiService;
 
-    public AiController(GeminiService geminiService) {
-        this.geminiService = geminiService;
+    public AiController(GeminiService aiService) {
+        this.aiService = aiService;
     }
 
     @PostMapping("/ask")
     public ResponseEntity<AiResponse> askAi(@RequestParam("text") String text) {
-        AiResponse response = geminiService.requestAi(text);
+        AiResponse response = aiService.requestAi(text);
 
         return ResponseEntity.ok(response);
     }
